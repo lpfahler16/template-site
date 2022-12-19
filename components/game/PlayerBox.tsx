3459142d435b1df3dbebbf958ecbef6e18c6248a
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2022-12-19 10:15:05 -0500

    Working?

--- components/game/PlayerBox.tsx
+++ components/game/PlayerBox.tsx
@@ -1,14 +1,35 @@
+import { TimerResult } from "react-timer-hook";
+import { Player } from "../../pages/game";
+
 interface PlayerBoxProps {
   className?: string;
+  player: Player;
+  playerClicked: (player: number) => void;
+  index: number; // Player number
 }
 
 const PlayerBox: React.FC<PlayerBoxProps> = ({
   className = "",
+  player,
+  playerClicked,
+  index,
 }): JSX.Element => {
+  const buttonColor = player.timer.isRunning ? "bg-main-red" : "";
+
   return (
-    <div className={"flex justify-center items-center flex-grow " + className}>
-      <p className="text-7xl text-white">3:00</p>
-    </div>
+    <button
+      onClick={() => playerClicked(index)}
+      className={
+        "flex justify-center items-center flex-grow " +
+        className +
+        " " +
+        buttonColor
+      }
+    >
+      <p className="text-7xl text-white w-full">
+        {player.timer.minutes}:{player.timer.seconds}
+      </p>
+    </button>
   );
 };
 
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,8 +1,35 @@
 import Head from "next/head";
 import { AiFillHome } from "react-icons/ai";
 import PlayerBox from "../components/game/PlayerBox";
+import { TimerResult, useTimer } from "react-timer-hook";
 
-export default function Coding() {
+export interface Player {
+  timer: TimerResult;
+}
+
+export default function Game() {
+  const withTimerLength = (length: number) => {
+    var d = new Date();
+    d.setSeconds(d.getSeconds() + length);
+    return d;
+  };
+
+  const timerSettings = () => ({
+    expiryTimestamp: withTimerLength(180),
+    autoStart: false,
+  });
+  const timer1 = useTimer(timerSettings());
+  const timer2 = useTimer(timerSettings());
+
+  const players: Player[] = [{ timer: timer1 }, { timer: timer2 }];
+
+  const playerClicked = async (player: number) => {
+    const clicker = players[player];
+    const other = players[(player + 1) % 2];
+    clicker.timer.pause();
+    other.timer.resume();
+  };
+
   return (
     <div>
       <Head>
@@ -13,14 +40,23 @@ export default function Coding() {
 
       <main className="bg-darkest-gray min-h-screen overflow-hidden">
         <div className="flex flex-col h-screen">
-          <PlayerBox className="rotate-180" />
+          <PlayerBox
+            index={0}
+            className="rotate-180"
+            player={players[0]}
+            playerClicked={playerClicked}
+          />
           <div className="h-20 bg-white flex justify-center items-center gap-8">
-            <div className="border border-black rounded-full py-2 px-4">
+            {/* <div className="border border-black rounded-full py-2 px-4">
               Hold to Pause
             </div>
-            <AiFillHome className="text-3xl" />
+            <AiFillHome className="text-3xl" /> */}
           </div>
-          <PlayerBox />
+          <PlayerBox
+            index={1}
+            player={players[1]}
+            playerClicked={playerClicked}
+          />
         </div>
       </main>
     </div>
